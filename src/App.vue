c02fcd33986065ea97abc204370a2021ed988120
Rafaél José <rafael.jferreira27@gmail.com>
2025-10-31 20:45:06 -0300

    To do list Trello

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container mt-5">
+    <div class="row">
+      <!-- Coluna 1 -->
+      <div class="col-12 col-md-4">
+        <h5 class="text-center">A Fazer</h5>
+        <Draggable v-model="todo" group="tasks" class="list-group">
+          <div
+            v-for="item in todo"
+            :key="item.id"
+            class="list-group-item"
+          >
+            {{ item.name }}
+          </div>
+        </Draggable>
+      </div>
+
+      <!-- Coluna 2 -->
+      <div class="col-12 col-md-4">
+        <h5 class="text-center">Em andamento</h5>
+        <Draggable v-model="doing" group="tasks" class="list-group">
+          <div
+            v-for="item in doing"
+            :key="item.id"
+            class="list-group-item"
+          >
+            {{ item.name }}
+          </div>
+        </Draggable>
+      </div>
+
+      <!-- Coluna 3 -->
+      <div class="col-12 col-md-4">
+        <h5 class="text-center">Concluído</h5>
+        <Draggable v-model="done" group="tasks" class="list-group">
+          <div
+            v-for="item in done"
+            :key="item.id"
+            class="list-group-item"
+          >
+            {{ item.name }}
+          </div>
+        </Draggable>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { VueDraggableNext as Draggable } from 'vue-draggable-next'
+
+const todo = ref([
+  { id: 1, name: 'Estudar Vue' },
+  { id: 2, name: 'Ler documentação' },
+])
+const doing = ref([
+  { id: 3, name: 'Testar grid Bootstrap' },
+])
+const done = ref([
+  { id: 4, name: 'Instalar dependências' },
+])
+</script>
+
+<style>
+.list-group {
+  min-height: 200px;
+  background-color: #f8f9fa;
+  padding: 10px;
+  border-radius: 8px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// IMPORTA o que você realmente usa no App.vue
+import { faBookmark, faTrash, faCheck } from '@fortawesome/free-solid-svg-icons'
+
+// adiciona na library
+library.add(faBookmark, faTrash, faCheck)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
